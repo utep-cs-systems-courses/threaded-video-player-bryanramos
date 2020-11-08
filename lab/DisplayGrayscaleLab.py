d79115b57ed257bf93f922e87c81db545aec3481
Bryan Ramos <ramos.bryan100@gmail.com>
2020-11-08 13:38:54 -0700

    Adding file for lab source

---
+++ lab/DisplayGrayscaleLab.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Author: Bryan Ramos
+# Course: Theory of Operating Systems (OS)
+# Instructors: Eric Freudenthal and David Pruitt
+# Assignment: Lab 2 - TCP File Transfer
+
+import cv2
+
+VIDEOFILE = "../clip.mp4"
+DELIMITER = "\0"
