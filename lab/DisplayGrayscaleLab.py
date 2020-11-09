e8169d042c8b3b53b49e66cf2993d45f7110b880
Bryan Ramos <ramos.bryan100@gmail.com>
2020-11-08 20:47:56 -0700

    Separating ThreadQueue class from rest of source

--- lab/DisplayGrayscaleLab.py
+++ lab/DisplayGrayscaleLab.py
@@ -7,32 +7,11 @@
 
 import cv2
 import threading
+from ThreadQueue import ThreadQueue
 
 VIDEOFILE = "../clip.mp4"
 DELIMITER = "\0"
 FRAMEDELAY = 42 # the answer to everything, as said in the demos
-
-class ThreadQueue(): # queue implemented with python list
-    def __init__(self):
-        self.queue = []
-        self.lock = threading.Lock()
-        self.full = threading.Semaphore(0)
-        self.empty = threading.Semaphore(24)
-
-    def put(self, item): # enqueue
-        self.empty.acquire()
-        self.lock.acquire()
-        self.queue.append(item)
-        self.lock.release()
-        self.full.release()
-
-    def obtain(self): # dequeue
-        self.full.acquire()
-        self.lock.acquire()
-        item = self.queue.pop(0)
-        self.lock.release()
-        self.empty.release()
-        return item
 
 # based on extractFrames.py demo
 def extractFrames(fileName, frameQueue):
